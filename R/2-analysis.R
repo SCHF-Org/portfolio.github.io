aec7c57cedd91ea0fcbecaf2321fd616dc1ceb8c
redapemusic35 <redapemusic35@gmail.com>
2026-03-21 07:43:05 -0400

    add

--- R/2-analysis.R
+++
@@ -1,8 +0,0 @@
-Data <- readRDS("data/Data.rds")
-
-Mods <- list()
-
-Mods$"Default" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale, data=Data)
-Mods$"Default + Noise" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale + noise, data=Data)
-
-saveRDS(Mods, "data/Mods.rds")
